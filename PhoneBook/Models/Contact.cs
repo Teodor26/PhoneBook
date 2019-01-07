d20e03bd8b043899abc2403faf9a9221c0401b06
Teodor26 <teodorbear26@gmail.com>
2019-01-07 17:58:50 +0200

    Changes

--- PhoneBook/Models/Contact.cs
+++ PhoneBook/Models/Contact.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhoneBook.Models
 {
@@ -9,11 +10,14 @@ namespace PhoneBook.Models
     {
 
         public int Id { get; set; }
-
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
-
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
-
+        [Required(ErrorMessage = "Поле должно быть установлено")]
+        [Display(Name = "Должность")]
         public string Position { get; set; }
 
         List<Phone> Phones = new List<Phone>();
